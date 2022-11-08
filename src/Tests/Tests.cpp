bb11bf273818dccc1a60f33c7b8610b05872d7d5
Almaz Azizov <105113852+s21violette@users.noreply.github.com>
2022-11-09 02:34:54 +1100

    Update Tests.cpp

--- src/Tests/Tests.cpp
+++ src/Tests/Tests.cpp
@@ -13,8 +13,10 @@ TEST(AntAlgorithmTests, Test) {
     s21::S21Matrix::FillMatrixWithRandValues(matrix1);
     s21::AntAlgorithm ant_solver.SetData(matrix1, 10);
 
-    TsmResult default_algo = ant_solver.SolveWithoutUsingParallelism();
-    TsmResult threading_algo = ant_colver.SolveWithUsingParallelism();
+    ant_solver.SolveWithoutUsingParallelism();
+    TsmResult default_algo = ant_solver.GetResult();
+    ant_solver.SolveWithUsingParallelism();
+    TsmResult threading_algo = ant_colver.GetResult();
     EXPECT_TRUE(default_algo.distance == threading_algo.distance);
 }
 
@@ -22,12 +24,14 @@ TEST(AntAlgorithmTests, Test) {
     s21::S21Matrix matrix1(10, 10);
     s21::S21Matrix::FillMatrixWithRandValues(matrix1);
     s21::AntAlgorithm ant_solver.SetData(matrix1, 5);
-    TsmResult default_algo = ant_solver.SolveWithoutUsingParallelism();
+    ant_solver.SolveWithoutUsingParallelism()
+    TsmResult default_algo = ant_solver.GetResult();
 
     s21::S21Matrix matrix2(3, 3);
     s21::S21Matrix::FillMatrixWithRandValues(matrix2);
     ant_solver_.SetData(matrix2, 5);
-    TsmResult threading_algo = ant_colver.SolveWithUsingParallelism();
+    ant_colver.SolveWithUsingParallelism();
+    TsmResult threading_algo = ant_colver.GetResult();
     EXPECT_FALSE(default_algo.distance == threading_algo.distance);
 }
 
@@ -36,8 +40,10 @@ TEST(AntAlgorithmTests, Test) {
     s21::S21Matrix::FillMatrixWithRandValues(matrix1);
     s21::AntAlgorithm ant_solver.SetData(matrix1, 3);
 
-    TsmResult default_algo = ant_solver.SolveWithoutUsingParallelism();
-    TsmResult threading_algo = ant_colver.SolveWithUsingParallelism();
+    ant_solver.SolveWithoutUsingParallelism();
+    TsmResult default_algo = ant_solver.GetResult();
+    ant_colver.SolveWithUsingParallelism();
+    TsmResult threading_algo = ant_colver.GetResult();
     EXPECT_TRUE(default_algo.distance == threading_algo.distance);
 }
 
