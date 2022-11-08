641dba3b7fe00e1755e801a05eacf779b5d69be9
violette <almaz.azizov.01@mail.ru>
2022-11-09 01:42:37 +1100

    added tests

--- src/Tests/Tests.cpp
+++ src/Tests/Tests.cpp
@@ -2,10 +2,44 @@
 
 #include <string>
 
+#include "../Algorithms/AntColonyAlgorithm/AntAlgorithm.h"
 #include "../Algorithms/GaussAlgorithm/GaussAlgorithm.h"
 #include "../Algorithms/WinogradAlgorithm/WinogradAlgorithm.h"
 #include "../ConsoleEngine/ConsoleForGauss/ConsoleForTestingGauss/ConsoleForTestingGauss.h"
 #include "../DataStructures/Matrix/Matrix.h"
+
+TEST(AntAlgorithmTests, Test) {
+    s21::S21Matrix matrix1(10, 10);
+    s21::S21Matrix::FillMatrixWithRandValues(matrix1);
+    s21::AntAlgorithm ant_solver.SetData(matrix1, 10);
+
+    TsmResult default_algo = ant_solver.SolveWithoutUsingParallelism();
+    TsmResult threading_algo = ant_colver.SolveWithUsingParallelism();
+    EXPECT_TRUE(default_algo.distance == threading_algo.distance);
+}
+
+TEST(AntAlgorithmTests, Test) {
+    s21::S21Matrix matrix1(10, 10);
+    s21::S21Matrix::FillMatrixWithRandValues(matrix1);
+    s21::AntAlgorithm ant_solver.SetData(matrix1, 5);
+    TsmResult default_algo = ant_solver.SolveWithoutUsingParallelism();
+
+    s21::S21Matrix matrix2(3, 3);
+    s21::S21Matrix::FillMatrixWithRandValues(matrix2);
+    ant_solver_.SetData(matrix2, 5);
+    TsmResult threading_algo = ant_colver.SolveWithUsingParallelism();
+    EXPECT_FALSE(default_algo.distance == threading_algo.distance);
+}
+
+TEST(AntAlgorithmTests, Test) {
+    s21::S21Matrix matrix1(1, 1);
+    s21::S21Matrix::FillMatrixWithRandValues(matrix1);
+    s21::AntAlgorithm ant_solver.SetData(matrix1, 3);
+
+    TsmResult default_algo = ant_solver.SolveWithoutUsingParallelism();
+    TsmResult threading_algo = ant_colver.SolveWithUsingParallelism();
+    EXPECT_TRUE(default_algo.distance == threading_algo.distance);
+}
 
 TEST(WinogradAlgoTests, EvenMatrices) {
     s21::S21Matrix m1(100, 100);
